ed888e002f4a7e75ec9da027839faf929a1e1991
Anwesh43 <anweshthecool0@gmail.com>
2018-03-18 11:09:52 +0530

    adding doublepieview module

--- doublepieview/src/main/java/ui/anwesome/com/doublepieview/DoublePieView.kt
+++ doublepieview/src/main/java/ui/anwesome/com/doublepieview/DoublePieView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.doublepieview
 /**
  * Created by anweshmishra on 18/03/18.
  */
+import android.content.*
+import android.graphics.*
+import android.view.*
+class DoublePieView (ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
