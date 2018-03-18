e8d306442f69f7b7619d00400ef523374b47f792
Anwesh43 <anweshthecool0@gmail.com>
2018-03-18 12:44:24 +0530

    adding DoublePieListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlindoublepieview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlindoublepieview/MainActivity.kt
@@ -3,13 +3,19 @@ package ui.anwesome.com.kotlindoublepieview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.doublepieview.DoublePieView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DoublePieView.create(this)
+        val view = DoublePieView.create(this)
+        view.addDoublePieListener({
+            Toast.makeText(this, "closed", Toast.LENGTH_SHORT).show()
+        }, {
+            Toast.makeText(this, "opened", Toast.LENGTH_SHORT).show()
+        })
         fullScreen()
     }
 }
