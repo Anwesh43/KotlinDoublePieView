d18cfe193d4ff5a82d69e5a12ce9e3798979c7c9
Anwesh43 <anweshthecool0@gmail.com>
2018-03-18 12:20:51 +0530

    adding DoublePieView in activity

--- app/src/main/java/ui/anwesome/com/kotlindoublepieview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlindoublepieview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlindoublepieview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.doublepieview.DoublePieView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DoublePieView.create(this)
     }
 }
